b5f20d574317860df8f3d1e39a4370fb5cf9a2bb
Kadyr Gullyyev <kadyr.gullyyew@gmail.com>
2025-03-11 16:29:56 -0700

    com

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,7 @@
-import {useTranslations} from 'next-intl';
 import { Services, Introduction, Blogs } from '@/components/shared';
 
 
 export default function HomePage() {
-    const t = useTranslations('HomePage');
     return (
         <>
             <Introduction />
